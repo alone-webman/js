a62ef5b1fa1b02c35f0c69c20bbe071263b21ad3
admin <my@admin.com>
2025-06-08 23:36:34 +0800

    .

--- function.php
+++ function.php
@@ -9,16 +9,19 @@ function alone_js_route(): void {
 
 
 /**
- * @param string $title 标题
- * @param string $body  内容
- * @param string $head  头部
- * @param string $entry 入口js
+ * @param string|array $title 标题
+ * @param string       $body  内容
+ * @param string       $head  头部
+ * @param string       $entry 入口js
  * @return Response
  */
-function alone_js_html(string $title, string $body, string $head = "", string $entry = ""): Response {
-    return response(Facade::tag(@file_get_contents(__DIR__ . '/index.html'), [
-        'title' => $title,
-        'head'  => $head,
-        'body'  => ($body . (!empty($js) ? '<script type="text/javascript" src="' . $entry . '"></script>' : "")),
-    ]));
+function alone_js_html(string|array $title, string $body = '<div id="app"></div>', string $head = "", string $entry = "/alone.js"): Response {
+    return response(Facade::tag(@file_get_contents(__DIR__ . '/index.html'), is_array($title)
+        ? array_merge($title, ['entry' => '<script type="text/javascript" src="' . $entry . '"></script>'])
+        : [
+            'title' => $title,
+            'head'  => $head,
+            'body'  => $body,
+            'entry' => '<script type="text/javascript" src="' . $entry . '"></script>',
+        ]));
 }
