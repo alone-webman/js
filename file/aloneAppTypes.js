f16d4d02e6cfb7fadd195d27c02b102d611c86cc
admin <my@admin.com>
2025-06-25 00:39:28 +0800

    .

--- file/aloneAppTypes.js
+++ file/aloneAppTypes.js
@@ -1,10 +1,72 @@
+/**
+ * === app配置 ===
+ * @typedef {Object} appConfig
+ * @property {string}        [elem]        容器（默认 app）
+ * @property {string}        [home]        路由主页（默认 index）
+ * @property {boolean}       [match]       任意路由开关（默认 true）
+ * @property {string}        [jump]        路由不存在或者出错时跳转（默认 /）
+ * @property {object}        [group]       路由分组（默认 {}）
+ * @property {object}        [i18n]        设置i18n  （默认 {} ）
+ * @property {object}        [lang]        i18n语文包（默认 {}） messages
+ * @property {object}        [router]      路由设置（默认 {}）
+ * @property {array}         [routes]      路由列表（默认 []）
+ * @property {number}        [routeType]   路由类型（默认 1）1=watch 2=watchEffect 3=isReady
+ * @property {string|null}   [app]         app主入口路径
+ * @property {string|null}   [main]        main配置文件路径
+ */
+/**
+ * === 路由配置 ===
+ * @typedef {Object} routeConfig
+ * @property {string}                        [view]        请求视图（默认 /）
+ * @property {string}                        [format]      请求格式（默认 vue）
+ * @property {number}                        [timeout]     超时时间（默认 20000）
+ * @property {boolean}                       [blob]        是否使用blob使用（默认 false）
+ * @property {boolean}                       [cache]       缓存开关（默认 true）
+ * @property {boolean|function(code,type)}   [compress]    是否压缩,可以以自定压缩
+ * @property {function(body,path,type,xhr)}  [reqBody]     请求后内容处理
+ * @property {string}                        [jsFormat]    js中的import没有格式是否补格式 (默认 js）可设js|vue等,不要.
+ * @property {string}                        [cssFormat]   css中的import没有格式是否补格式 (默认 css） 可设css等,不要.
+ * @property {string|array|null}             [importType]  直接import的类型（默认 null）如js:application/javascript
+ * @property {string}                        [alone]       default中的参数名称（默认 alone) 可设置title/description/keywords/favicon/link/script//head[name:{tag:"",content:"",attr:{}}]
+ * @property {number}                        [delay]       状态设置 延迟显示（ms，默认 300）
+ * @property {function}                      [loading]     路由加载中处理包,返回显示的内容（默认 null）
+ * @property {function}                      [error]       路由加载错误处理包,返回显示的内容（默认 null）
+ */
 window.aloneApp = {
     /**
      * app初始化
      * @param {appConfig&routeConfig} option
      *
      */
-    app: (option = {}) => option,
+    app(option = {
+        app: null,
+        main: null,
+        elem: "app",
+        home: "index",
+        match: true,
+        jump: "/",
+        group: null,
+        i18n: {},
+        lang: {},
+        router: null,
+        routes: [],
+        routeType: 1,
+        view: "/view/",
+        format: "vue",
+        timeout: 20000,
+        blob: false,
+        cache: true,
+        compress: true,
+        reqBody: (body) => body,
+        jsFormat: "js",
+        cssFormat: "css",
+        delay: 200,
+        loading: null,
+        error: null,
+        importType: null,
+        alone: "alone"
+    } = {}) {
+    },
     vue: Vue,
     vueRouter: VueRouter,
     vueI18n: VueI18n,
@@ -116,6 +178,113 @@ window.aloneApp = {
      * @returns {object}
      */
     async getVue(path) {
+    },
+    routeWay: {
+        /**
+         * 分组路由配置
+         * @param {object|null}     [group]     分组列表，支持无限级嵌套
+         * @param {function|object} [callback]  组件处理回调配置
+         * @param {string}          [prefix=""] 路由名称前缀
+         * @returns {array}
+         */
+        groupRoute(group, callback, prefix = "") {
+        },
+        /**
+         * 组件包
+         * @param {string}                     path   路径
+         * @param {function(path)}             loader 加载包
+         * @param {number|null}                [delay]
+         * @param {number|null}                [timeout]
+         * @param {function(path)|null}        [loading]
+         * @param {function(path,error)|null}  [error]
+         * @returns {{template: string, components: {aloneTemp}}|{errTemp}|{loadTemp}}
+         */
+        component(path, loader, {delay = null, timeout = null, loading = null, error = null} = {}) {
+        },
+        /**
+         * 组件包
+         * @param {string}                     path   路径
+         * @param {function(path)}             loader 加载包
+         * @param {number|null}                [delay]
+         * @param {number|null}                [timeout]
+         * @param {function(path)|null}        [loading]
+         * @param {function(path,error)|null}  [error]
+         * @returns {{template: string, components: {aloneTemp}}|{errTemp}|{loadTemp}}
+         */
+        components(path, loader, {delay = null, timeout = null, loading = null, error = null} = {}) {
+        }
+    },
+    /**
+     *  这是crypto
+     */
+    crypto: crypto,
+    safe: {
+        crypto: aloneApp.crypto,
+        utils: {
+            rand() {
+            },
+            to_json(data) {
+            },
+            to_obj(data) {
+            },
+            mov(name) {
+            },
+            mov_rand(name) {
+            }
+        },
+        md5(data) {
+        },
+        sha1(data) {
+        },
+        sha3(data) {
+        },
+        sha256(data) {
+        },
+        sha512(data) {
+        },
+        base64: {},
+        aes: {
+            encrypt(data, key, iv) {
+            },
+            decrypt(data, key, iv) {
+            },
+            en(data) {
+            },
+            de(data, random) {
+            }
+        },
+        des: {
+            encrypt(data, key, iv) {
+            },
+            decrypt(data, key, iv) {
+            },
+            en: (data) => {
+            },
+            de(data, random) {
+            }
+        },
+        des3: {
+            encrypt(data, key, iv) {
+            },
+            decrypt(data, key, iv) {
+            },
+            en(data) {
+            },
+            de(data, random) {
+            }
+        },
+        mov: {
+            en(data, mode) {
+            },
+            de(data, random) {
+            }
+        },
+        url: {
+            en(data, mode) {
+            },
+            de(data) {
+            }
+        }
     },
     utils: {
         /**
@@ -253,155 +422,4 @@ window.aloneApp = {
         getParams(url = "", params = {}) {
         },
     },
-    /**
-     *  这是crypto
-     */
-    crypto: crypto,
-    safe: {
-        crypto: aloneApp.crypto,
-        utils: {
-            rand() {
-            },
-            to_json(data) {
-            },
-            to_obj(data) {
-            },
-            mov(name) {
-            },
-            mov_rand(name) {
-            }
-        },
-        md5(data) {
-        },
-        sha1(data) {
-        },
-        sha3(data) {
-        },
-        sha256(data) {
-        },
-        sha512(data) {
-        },
-        base64: {},
-        aes: {
-            encrypt(data, key, iv) {
-            },
-            decrypt(data, key, iv) {
-            },
-            en(data) {
-            },
-            de(data, random) {
-            }
-        },
-        des: {
-            encrypt(data, key, iv) {
-            },
-            decrypt(data, key, iv) {
-            },
-            en: (data) => {
-            },
-            de(data, random) {
-            }
-        },
-        des3: {
-            encrypt(data, key, iv) {
-            },
-            decrypt(data, key, iv) {
-            },
-            en(data) {
-            },
-            de(data, random) {
-            }
-        },
-        mov: {
-            en(data, mode) {
-            },
-            de(data, random) {
-            }
-        },
-        url: {
-            en(data, mode) {
-            },
-            de(data) {
-            }
-        }
-    },
-    routeWay: {
-        /**
-         * 分组路由配置
-         * @param {object|null}     [group]     分组列表，支持无限级嵌套
-         * @param {function|object} [callback]  组件处理回调配置
-         * @param {string}          [prefix=""] 路由名称前缀
-         * @returns {array}
-         */
-        groupRoute(group, callback, prefix = "") {
-        },
-        /**
-         * 组件包
-         * @param {string}                     path   路径
-         * @param {function(path)}             loader 加载包
-         * @param {number|null}                [delay]
-         * @param {number|null}                [timeout]
-         * @param {function(path)|null}        [loading]
-         * @param {function(path,error)|null}  [error]
-         * @returns {{template: string, components: {aloneTemp}}|{errTemp}|{loadTemp}}
-         */
-        component(path, loader, {
-            delay = null,
-            timeout = null,
-            loading = null,
-            error = null
-        } = {}) {
-        },
-        /**
-         * 组件包
-         * @param {string}                     path   路径
-         * @param {function(path)}             loader 加载包
-         * @param {number|null}                [delay]
-         * @param {number|null}                [timeout]
-         * @param {function(path)|null}        [loading]
-         * @param {function(path,error)|null}  [error]
-         * @returns {{template: string, components: {aloneTemp}}|{errTemp}|{loadTemp}}
-         */
-        components(path, loader, {
-            delay = null,
-            timeout = null,
-            loading = null,
-            error = null
-        } = {}) {
-        }
-    },
 };
-/**
- * === app配置 ===
- * @typedef {Object} appConfig
- * @property {string}        [elem]        容器（默认 app）
- * @property {string}        [home]        路由主页（默认 index）
- * @property {boolean}       [match]       任意路由开关（默认 true）
- * @property {string}        [jump]        路由不存在或者出错时跳转（默认 /）
- * @property {object}        [group]       路由分组（默认 {}）
- * @property {object}        [i18n]        设置i18n  （默认 {} ）
- * @property {object}        [lang]        i18n语文包（默认 {}） messages
- * @property {object}        [router]      路由设置（默认 {}）
- * @property {array}         [routes]      路由列表（默认 []）
- * @property {number}        [routeType]   路由类型（默认 1）1=watch 2=watchEffect 3=isReady
- * @property {string|null}   [app]         app主入口路径
- * @property {string|null}   [main]        main配置文件路径
- */
-/**
- * === 路由配置 ===
- * @typedef {Object} routeConfig
- * @property {string}                        [view]        请求视图（默认 /）
- * @property {string}                        [format]      请求格式（默认 vue）
- * @property {number}                        [timeout]     超时时间（默认 20000）
- * @property {boolean}                       [blob]        是否使用blob使用（默认 false）
- * @property {boolean}                       [cache]       缓存开关（默认 true）
- * @property {boolean|function(code,type)}   [compress]    是否压缩,可以以自定压缩
- * @property {function(body,path,type,xhr)}  [reqBody]     请求后内容处理
- * @property {string}                        [jsFormat]    js中的import没有格式是否补格式 (默认 js）可设js|vue等,不要.
- * @property {string}                        [cssFormat]   css中的import没有格式是否补格式 (默认 css） 可设css等,不要.
- * @property {string|array|null}             [importType]  直接import的类型（默认 null）如js:application/javascript
- * @property {string}                        [alone]       default中的参数名称（默认 alone) 可设置title/description/keywords/favicon/link/script//head[name:{tag:"",content:"",attr:{}}]
- * @property {number}                        [delay]       状态设置 延迟显示（ms，默认 300）
- * @property {function}                      [loading]     路由加载中处理包,返回显示的内容（默认 null）
- * @property {function}                      [error]       路由加载错误处理包,返回显示的内容（默认 null）
- */
